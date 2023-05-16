3fa00d32680d21da965ac8dc336ab19ab843a948
AlekseyKashubin <Akashubin@yahoo.com>
2023-05-15 21:38:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function removeButton(element){
+element.remove();
+}
+
+function ninjaLikes(likes){
+    return 'Ninja was liked!';
+}
+ninjaLikes("Ninja was liked");
+
+function login(logout){
+logout.innerText = 'Logout'
+}
