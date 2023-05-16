926f7d15cae1439358cabd1b6815fc28084d51dd
AlekseyKashubin <Akashubin@yahoo.com>
2023-05-15 21:42:31 -0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function removeButton(element){
-element.remove();
+
+// function for add definition button removal
+function removeButton(element) {
+    element.remove();
 }
 
-function ninjaLikes(likes){
+// function for like button 
+function ninjaLikes(likes) {
     return 'Ninja was liked!';
 }
 ninjaLikes("Ninja was liked");
 
-function login(logout){
-logout.innerText = 'Logout'
+
+// function for logout button 
+function login(logout) {
+    logout.innerText = 'Logout'
 }
